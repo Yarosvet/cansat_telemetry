043dbad1331da448bbc77e4a91ac5cbbfc8c6432
Yarosvet <yarosvetk@yandex.ru>
2022-05-28 11:51:18 +0300

    String changed, UI edited, etc

--- rx_serial.py
+++ rx_serial.py
@@ -10,6 +10,7 @@ class SerialReader:
     def __init__(self):
         self.com_port = "COM11"
         self.serial_port = 9600
+        self.alive = True
         # Values
         self.time_start = 0
         self.pressure = 0
@@ -32,6 +33,8 @@ class SerialReader:
         self.humidity = 0
         self.methane = 0
         self.hydrogen = 0
+        self.gps_lat = 0
+        self.gps_lon = 0
         # Data sets
         self.presstemp_data = []
         self.presstemp_imu_data = []
@@ -52,29 +55,58 @@ class SerialReader:
         self.rx_thread = Thread(target=self.imitate_rx, name="rxThread", daemon=True)
         self.rx_thread.start()
 
-    def update_from_str(self, s):  # "1000 101325 0 0 -9 25.5 101325 0 0 -9 25.5 9 9 9 0 0 200 3.75 46 500 600"
-        cc = list(map(float, s.split()))
-        self.time_start = cc[0] / 1000
-        self.pressure = cc[1]
-        self.acc_ax = cc[2]
-        self.acc_ay = cc[3]
-        self.acc_az = cc[4]
-        self.temperature = cc[5]
-        self.pressure_imu = cc[6]
-        self.acc_ax_imu = cc[7]
-        self.acc_ay_imu = cc[8]
-        self.acc_az_imu = cc[9]
-        self.temperature_imu = cc[10]
-        self.mag_mx = cc[11]
-        self.mag_my = cc[12]
-        self.mag_mz = cc[13]
-        self.gyr_x = cc[14]
-        self.gyr_y = cc[15]
-        self.gyr_z = cc[16]
-        self.brightness = cc[17]
-        self.humidity = cc[18]
-        self.methane = cc[19]
-        self.hydrogen = cc[20]
+    def update_from_str(self, s):
+        # "time=1000 pressure=101325 acc_ax=0 acc_ay=0 acc_az=-9 temperature=25.5 pressure_imu=101325 acc_ax_imu=0
+        # acc_ay_imu=0 acc_az_imu=-9 temperature_imu=25.5 mag_mx=9 mag_my=9 mag_mz=9 gyr_x=0 gyr_y=0 gyr_z=200
+        # brightness=3.75 humidity=46 methane=500 hydrogen=600 gps_lat=54.22 gps_lon=36.55"
+        kvalues = list(map(lambda x: (x.split("=")[0], float(x.split("=")[1])), s.split()))
+        for k, v in kvalues:
+            if k == "time":
+                self.time_start = v / 1000
+            elif k == "pressure":
+                self.pressure = v
+            elif k == "acc_ax":
+                self.acc_ax = v
+            elif k == "acc_ay":
+                self.acc_ay = v
+            elif k == "acc_az":
+                self.acc_az = v
+            elif k == "temperature":
+                self.temperature = v
+            elif k == "pressure_imu":
+                self.pressure_imu = v
+            elif k == "acc_ax_imu":
+                self.acc_ax_imu = v
+            elif k == "acc_ay_imu":
+                self.acc_ay_imu = v
+            elif k == "acc_az_imu":
+                self.acc_az_imu = v
+            elif k == "temperature_imu":
+                self.temperature_imu = v
+            elif k == "mag_mx":
+                self.mag_mx = v
+            elif k == "mag_my":
+                self.mag_my = v
+            elif k == "mag_mz":
+                self.mag_mz = v
+            elif k == "gyr_x":
+                self.gyr_x = v
+            elif k == "gyr_y":
+                self.gyr_y = v
+            elif k == "gyr_z":
+                self.gyr_z = v
+            elif k == "brightness":
+                self.brightness = v
+            elif k == "humidity":
+                self.humidity = v
+            elif k == "methane":
+                self.methane = v
+            elif k == "hydrogen":
+                self.hydrogen = v
+            elif k == "gps_lat":
+                self.gps_lat = v
+            elif k == "gps_lon":
+                self.gps_lon = v
         # Updating datasets
         self.presstemp_data.append([self.temperature, self.pressure])
         self.presstemp_data.sort()
@@ -93,16 +125,23 @@ class SerialReader:
 
     def rx(self):
         ser = serial.Serial(self.com_port, self.serial_port)
-        while True:
-            self.update_from_str(str(ser.readline()))
+        try:
+            while True:
+                self.update_from_str(ser.readline().decode("utf-8"))
+        except serial.serialutil.SerialException:
+            self.alive = False
 
     def imitate_rx(self):
         time_start = 0
         while True:
             time_start += 32
-            s = " ".join(map(str, [time_start, randint(90000, 102000), randint(-9, 0), randint(-9, 0), randint(-9, 0),
-                                   randint(-90, 250) / 10, randint(90000, 102000), randint(-9, 0), randint(-9, 0),
-                                   randint(-9, 0), randint(-90, 250) / 10, 9, 9, 9, randint(0, 15), randint(0, 15),
-                                   randint(0, 15), randint(100, 500) / 100, randint(10, 40), 500, 400]))
+            s = f"time={time_start} pressure={randint(90000, 102000)} acc_ax={randint(-9, 0)}" \
+                f" acc_ay={randint(-9, 0)} acc_az={randint(-9, 0)} " \
+                f"temperature={randint(-90, 250) / 10} pressure_imu={randint(90000, 102000)}" \
+                f" acc_ax_imu={randint(-9, 0)} acc_ay_imu={randint(-9, 0)} acc_az_imu={randint(-9, 0)}" \
+                f" temperature_imu={randint(-90, 250) / 10} mag_mx={9} mag_my={9} mag_mz={9}" \
+                f" gyr_x={randint(0, 15)} gyr_y={randint(0, 15)} gyr_z={randint(0, 15)}" \
+                f" brightness={randint(100, 500) / 100} humidity={randint(10, 40)} methane={500} hydrogen={400}" \
+                f" gps_lat={54 + randint(1, 99) / 100} gps_lon={36 + randint(1, 99) / 100}"
             self.update_from_str(s)
             sleep(0.032)
